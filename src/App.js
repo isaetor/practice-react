1db2a0c273e312937115f8a92260a78e60ceef20
Saeed Torkaman <saeedtorkaman15@gmail.com>
2023-11-30 13:56:14 +0330

    user State

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 // import logo from './assets/images/logo.svg';
+import { useState } from 'react';
 import './assets/css/App.css';
 
 function App() {
-  const name = "saeed"
-  return (
-    <Job salary={125000} position="Developer" company="google" />
-  )
-}
+  const [ count, setCount ] = useState(0)
 
-const Job = (props)=>{
+  const decrease = ()=>{
+    setCount(count > 0 ? count-1 : 0)
+  }
+  
   return (
     <div>
-      <h1>{props.salary}</h1>
-      <p>{props.position}</p>
-      <span>{props.company}</span>
+        <button onClick={()=>{setCount(count+1)}}>increat</button>
+        <button onClick={decrease}>decrease</button>
+        <button onClick={()=>{setCount(0)}}>set to 0</button>
+        <h1>{count}</h1>
     </div>
   )
 }
+
 
 export default App;
