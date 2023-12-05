3d22039334c3e3a2f1ed1f2b297c71c98917f978
Saeed Torkaman <saeedtorkaman15@gmail.com>
2023-12-05 23:36:16 +0330

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,95 +1,36 @@
-// import logo from './assets/images/logo.svg';
-import { useState } from 'react';
-import './assets/css/App.css';
+import axios from "axios";
+import "./assets/css/App.css"
+import { useEffect, useState } from "react";
 
-function App() {
-  const [ input, setInput ] = useState("")
-  const [ todoList, setTodoList ] = useState([])
-  const [ isEdit, setIsEdit ] = useState(false)
-  const [ editId, setEditId ] = useState(null)
+function App(){
 
-  const addTodo = () => {
-    if(input){
-      const item = {
-        id: todoList.length === 0 ? 1 : todoList[0].id + 1,
-        text: input,
-        isComplete: false
-      }
-      setTodoList([item,...todoList])
-      setInput("")
-    }
-  }
+  const [users, setUsers] = useState([])
 
-  const deleteTodo = (id) => {
-    setTodoList(todoList.filter((item)=> item.id !== id ))
-  }
+  useEffect(()=>{   
 
-  const editTodo = (id) => {
-    setIsEdit(true)
-    setEditId(id)
-    setInput(todoList.filter((item)=> item.id === id)[0].text)
-  }
-
-  const updateTodo = (id) => {
-    setTodoList(todoList.map(
-        (item)=>{
-          if(item.id === id) return {...item, text: input}
-          else return item
-        }
-      )
-    )
-    setIsEdit(false)
-    setEditId(null)
-    setInput("")
-  }
-  const complete = (id)=>{
-    setTodoList(todoList.map(
-      (item)=>{
-        if(item.id === id) return {...item, isComplete: !item.isComplete}
-        else return item
-      }
-    )
-  )
+    axios.get("https://randomuser.me/api/").then((res)=>{
+      setUsers(res.data.results)
+    })
+    
+  }, [])
+  
+  const getUsers = ()=>{
+    axios.get("https://randomuser.me/api/").then((res)=>{
+      setUsers(res.data.results)
+    })
   }
 
   return (
-    <div className="todo-app">
-      <div className="input-section">
-        <input type="text" placeholder="Add item..." onChange={(e)=>{ setInput(e.target.value) }} value={input}></input>
-        {
-          isEdit ? <div className="input-section">
-                      <button onClick={() => updateTodo(editId)}>Update</button>
-                      <button onClick={() => {setIsEdit(false); setEditId(null); setInput("");}}>Close</button>
-                    </div> 
-          : <button onClick={addTodo}>Add</button>
-        }
-      </div>
-      <div className="todos">
-        {
-          todoList.length > 0 ? <ul className="todo-list">
-            {
-              todoList.map((item, index)=>{
-                return (
-                  <li key={index} className="li">
-                    <div className='input-section'>
-                      <label className="container">
-                        <input type="checkbox" onChange={() => complete(item.id)} checked={item.isComplete} />
-                        <span className="checkmark"></span>
-                      </label>
-                      <span className="todo-text" style={{color: item.isComplete && "#555", textDecoration: item.isComplete && "line-through"}}>{item.text}</span>
-                    </div>
-                    <div className='input-section'>
-                      <button className="span-button" onClick={() => deleteTodo(item.id)}>Delete</button>
-                      <button className="span-button" onClick={() => editTodo(item.id)}>Edit</button>
-                    </div>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          : <h1 className="not-found"> NOT FOUND</h1>
-        }
-      </div>
+    <div>
+      <button onClick={getUsers}>new user</button>
+      { 
+        users?.map((user) => (
+          <div>
+            <p>{user.name.first+" "+user.name.last}</p>
+            <img src={user.picture.medium} alt={user.name.first}></img>
+          </div>
+        ))
+      }
     </div>
   )
 }
