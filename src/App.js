ff49894fa4344d51bcda6d10cdcfb0b7ec1dd85e
Saeed <saeedtorkaman15@gamil.com>
2023-12-07 19:54:56 +0330

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import axios from "axios";
 import "./assets/css/App.css"
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Shop } from "./pages/Shop"
+import { NotFound } from "./pages/NotFound"
 
 function App(){
-
-  const [users, setUsers] = useState([])
-
-  useEffect(()=>{   
-
-    axios.get("https://randomuser.me/api/").then((res)=>{
-      setUsers(res.data.results)
-    })
-    
-  }, [])
-  
-  const getUsers = ()=>{
-    axios.get("https://randomuser.me/api/").then((res)=>{
-      setUsers(res.data.results)
-    })
-  }
-
   return (
-    <div>
-      <button onClick={getUsers}>new user</button>
-      { 
-        users?.map((user) => (
-          <div>
-            <p>{user.name.first+" "+user.name.last}</p>
-            <img src={user.picture.medium} alt={user.name.first}></img>
-          </div>
-        ))
-      }
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+export const NotFound = ()=>{
+    return (
+    <center>
+       <h1>NotFound | 404</h1>
+    </center>
+    )
+}
