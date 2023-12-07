c684128a34eed90b34016915e010974133eccca2
Saeed <saeedtorkaman15@gamil.com>
2023-12-07 20:45:45 +0330

    create custom hook toggle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
+import { useToggle } from "../hooks/useToggle"
 
 export const Home = ()=>{
+    const [isVisible, toggle] = useToggle()
     return (
     <div>
-        Home
+        <button onClick={toggle}>{ isVisible ? "Hide" : "Show"}</button>
+        {isVisible && <h1>This is my hidden text</h1>}
     </div>
     )
 }
